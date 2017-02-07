535d63541ea98fd661fa601eb094670e550a6d4a
Davide Albieni <dalbieni@geiweb.it>
2017-02-07 17:26:31 +0100

    Drop function improved

--- Drop MrWolf world.sql
+++ Drop MrWolf world.sql
@@ -1,6 +1,42 @@
--- DROP MRWOLF
-DROP FUNCTION [mrwolf].[fn_concat_column_names_fk]
-DROP TABLE [mrwolf].[tbl_scripts]
-DROP SCHEMA [mrwolf]
+/*-- ==================================================================================================
+-- Name: MRWOLF-UTILITIES
+-- Author:		Jakodev
+-- Create date: JAN-2017
+-- Last Update: JAN-2017
+-- Description:	
+Drop all the objects belonging to [mrwolf] schema
+===================================================================================================== */
 
+DECLARE @schema varchar(128) = 'mrwolf'
+DECLARE @object_name varchar(128)
+DECLARE @object_type varchar(2)
+DECLARE @sql varchar(max)
 
+DECLARE drop_cursor CURSOR FOR select name, type from sys.objects where SCHEMA_NAME(schema_id) = @schema and type in ('FN', 'P', 'U')
+
+OPEN drop_cursor
+FETCH NEXT FROM drop_cursor INTO @object_name, @object_type
+WHILE (@@FETCH_STATUS = 0)
+BEGIN
+
+	SET @sql =
+		CASE @object_type
+			WHEN 'FN' THEN 'DROP FUNCTION {schema}.{object}'
+			WHEN 'P' THEN 'DROP PROCEDURE {schema}.{object}'
+			WHEN 'U' THEN 'DROP TABLE {schema}.{object}'
+			ELSE null
+		END
+
+	IF @sql is not null
+		BEGIN
+			SET @sql = REPLACE(@sql, '{schema}', @schema)
+			SET @sql = REPLACE(@sql, '{object}', @object_name)
+			EXEC sp_sqlexec @sql
+		END	
+
+	FETCH NEXT FROM drop_cursor INTO @object_name, @object_type
+
+END
+
+CLOSE drop_cursor
+DEALLOCATE drop_cursor
